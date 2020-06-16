8c20c8a1976ce85547e56e484c72e3a08f690f7d
LasPavos <pawelolkowicz9@gmail.com>
2020-06-16 22:25:14 +0100

    Questions are set up

--- App/app/src/main/java/com/example/uni21/SelectModuleActivity.java
+++ App/app/src/main/java/com/example/uni21/SelectModuleActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SelectModuleActivity extends AppCompatActivity {
     @Override
@@ -12,8 +13,10 @@ public class SelectModuleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_module);
     }
-    public void navigateTo(View view){
+    public void navigateTo(View view) {
         Intent intent = new Intent(this, StartingScreenActivity.class);
         startActivity(intent);
+        Button btn1 = findViewById(R.id.imageButton1);
+        btn1.setBackgroundResource(R.drawable.button_pressed_layout);
     }
 }
