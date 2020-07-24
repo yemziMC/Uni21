ebd2fb5f33a877f023140f8be5e5ebc3a6a25421
Matthew Clarke <matthew.clarke43@mail.dcu.ie>
2020-07-24 20:47:04 +0100

    login added and firebase completed

---
+++ App/app/src/main/java/com/example/uni21/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.uni21;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void navigateTo(View view){
+        Intent intent = new Intent(this, LandingActivity.class);
+        startActivity(intent);
+        Button btn1 = findViewById(R.id.loginbtn);
+    }
+}
